6a66aa48928defa941edff868b609170acd39da9
Manish Chand <manishchandr349@gmail.com>
2023-03-24 10:57:57 +0530

    add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let typed = new Typed(".type",{
-     strings : ["GitHub guy","Programmer","Developer"],
+     strings : ["GitHub guy","Programmer","Developer","GitHub guy"],
      typeSpeed: 150,
-     backSpeed: 100,
+     backSpeed: 200,
      looped: true
 })
